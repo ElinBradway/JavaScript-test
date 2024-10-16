78ee15e72278cb79ea45e089ffe9063c2c3b38ae
gitelinbradway <elinbradway@gmail.com>
2024-10-16 08:55:37 +0200

    All assignments ready

---
+++ main.js
@@ -0,0 +1,15 @@
+console. log('Hello World');
+
+let name = 'Elin';
+let age = '31';
+let color = 'green';
+
+console. log('Hell, my name is ' + name + ', I am '+ age + ', my favorite color is ' + color + '.');
+
+// Hope this is what the third assignment was about!
+let y= 9;
+let x=4;
+
+fruit = (x-y+(x/2)*100);
+
+console.log('Amount of fruit is '+ fruit);
